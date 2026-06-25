06cb1f14c287dc64c5b7615a5973f9eabf35840d
Rudra <rudra.agrawal321@gmail.com>
2026-06-25 22:52:33 +0530

    done

--- projects/asteroids/asteroid.py
+++ projects/asteroids/asteroid.py
@@ -1,7 +1,9 @@
 from typing import override
 from circleshape import *
 import pygame
-from constants import LINE_WIDTH
+import random
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x: float, y: float, radius: float) -> None:
@@ -15,4 +17,21 @@ class Asteroid(CircleShape):
     def update(self, dt):
         self.position += self.velocity * dt
         
+    def split(self) -> None:
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
         
+        log_event("asteroid_split")
+        angle = random.uniform(20, 50)
+        
+        vector_1 = self.velocity.rotate(angle)
+        vector_2 = self.velocity.rotate(-angle)
+        
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        
+        asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+        
+        asteroid_1.velocity = vector_1 * 1.2
+        asteroid_2.velocity = vector_2 * 1.2
--- projects/asteroids/main.py
+++ projects/asteroids/main.py
@@ -44,10 +44,15 @@ def main():
                 print("Game over!")
                 sys.exit()
                 
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.split()
         for object in drawable:
             object.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 100
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
